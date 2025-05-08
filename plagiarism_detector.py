48a4a913e3fcf3c921f148435b591cf068adc45d
Alejandro Moctezuma <moctezumaalejandro25@gmail.com>
2025-05-08 17:11:33 -0600

    Fork code from Lexical Analizer repository

--- plagiarism_detector.py
+++ plagiarism_detector.py
@@ -4,12 +4,37 @@
 ## A01736346 - Augusto Gómez Maxil
 ## A01736353 - Alejandro Daniel Moctezuma Cruz
 
-#BDD Entrenamiento
+##LIBRERÍAS
+import os, glob
 
-#BDD Clasificación
+##FUNCIONES
 
+#Algoritmos
+
+
+#Ejecución principal
 def main():
     print("Detector de Plagio utilizando Machine Learning")
+
+    #Abrir BDD de Entrenamiento
+    base_path = os.path.dirname(__file__)
+    data_dir = os.path.join(base_path, 'Data', 'Training')
+    archivos_entrenamiento = sorted(glob.glob(os.path.join(data_dir, '*.py')))
+    print(f"Se encontraron {len(archivos_entrenamiento)} archivos para entrenamiento.\n")
+
+    #preprocesar bdd
+        #reemplazar nombres de vars
+        #obtener palabras reservadas y cuantas veces aparecen?
+
+
+    #entrenar modelo con bdd
+
+    #Abrir BDD de Clasificación
+    data_dir = os.path.join(base_path, 'Data', 'Classification')
+    archivos_clasificación = sorted(glob.glob(os.path.join(data_dir, '*.py')))
+    print(f"Se encontraron {len(archivos_clasificación)} archivos para entrenamiento.\n")
+
+    #Pasar BDD_C por algoritmos
 
 if __name__ == '__main__':
     main()
