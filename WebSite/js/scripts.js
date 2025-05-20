e349dd4ae2ca007ba459a389be242703f5c46329
AugustoGM <A01736346@tec.mx>
2025-05-20 12:27:23 -0600

    Firt interface Index.html done

---
+++ WebSite/js/scripts.js
@@ -0,0 +1,9 @@
+function goTo(action) {
+  if (action === 'comparar') {
+    alert("Navegar a la interfaz de comparación.");
+    // location.href = "comparar.html";
+  } else if (action === 'subir') {
+    alert("Navegar a la interfaz de subida.");
+    // location.href = "subir.html";
+  }
+}
